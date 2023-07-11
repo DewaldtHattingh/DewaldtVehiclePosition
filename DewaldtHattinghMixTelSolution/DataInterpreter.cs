bf265ebc3507ba9016cb5565847f880253dcc88c
dewaldthattingh <dewaldthattingh@gmail.com>
2023-07-11 07:11:38 +0200

    Fixed data extraction

--- DewaldtHattinghMixTelSolution/DataInterpreter.cs
+++ DewaldtHattinghMixTelSolution/DataInterpreter.cs
@@ -1,73 +1,51 @@
+using System.Collections.Concurrent;
+using System.IO.MemoryMappedFiles;
 using System.Reflection;
 using System.Text;
 
 namespace DewaldtHattinghMixTelSolution;
 
- public static class DataInterpreter
+public static class DataInterpreter
+{
+    public static List<VehiclePositionData> ParseData()
     {
-        private static string GetStringFromBinary(BinaryReader readerInstance)
+        string directoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+        string fullPath = Path.Combine(directoryPath, "VehiclePositions.dat");
+
+        List<VehiclePositionData> vehiclePositions = new List<VehiclePositionData>(2000000);
+
+        using (FileStream fileStream = new FileStream(fullPath, FileMode.Open))
         {
-            var byteDataList = new List<byte>();
-            byte currentByte;
-            while ((currentByte = readerInstance.ReadByte()) != 0)
+            using (BinaryReader binaryReader = new BinaryReader(fileStream))
             {
-                byteDataList.Add(currentByte);
-            }
-
-            return Encoding.UTF8.GetString(byteDataList.ToArray());
-        }
-
-        public static List<VehiclePositionData> ParseData()
-        {
-            var vehiclePositionList = new List<VehiclePositionData>();
-            try
-            {
-                string directoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
-                string fullPath = Path.Combine(directoryPath, "VehiclePositions.dat");
-
-                using var fileStreamInstance = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
-                using var readerInstance = new BinaryReader(new BufferedStream(fileStreamInstance));
-
-                int definedChunkSize = 4096;
-                var dataBuffer = new byte[definedChunkSize];
-                int numBytesRead;
-                while ((numBytesRead = readerInstance.Read(dataBuffer, 0, dataBuffer.Length)) > 0)
+                while (binaryReader.BaseStream.Position < binaryReader.BaseStream.Length)
                 {
-                    using var memoryStreamInstance = new MemoryStream(dataBuffer, 0, numBytesRead);
-                    using var binaryReaderInstance = new BinaryReader(memoryStreamInstance);
-                    try
-                    {
-                        while (memoryStreamInstance.Position < memoryStreamInstance.Length)
-                        {
-                            var positionId = binaryReaderInstance.ReadInt32();
-                            var vehicleReg = GetStringFromBinary(binaryReaderInstance);
-                            var latVal = binaryReaderInstance.ReadSingle();
-                            var lonVal = binaryReaderInstance.ReadSingle();
-                            var utcTimeRecorded = binaryReaderInstance.ReadUInt64();
+                    VehiclePositionData positionData = new VehiclePositionData();
 
-                            var vehiclePositionData = new VehiclePositionData()
-                            {
-                                PositionId = positionId,
-                                VehicleRegistration = vehicleReg,
-                                Latitude = latVal,
-                                Longitude = lonVal,
-                                RecordedTimeUTC = utcTimeRecorded
-                            };
-                            vehiclePositionList.Add(vehiclePositionData);
-                        }
-                    }
-                    catch (EndOfStreamException exception)
-                    {
-                        Console.WriteLine("DataInterpreter.cs: " + exception.Message);
-                        break;
-                    }
+                    positionData.PositionId = binaryReader.ReadInt32();
+                    positionData.VehicleRegistration = GetStringFromBinary(binaryReader);
+                    positionData.Latitude = binaryReader.ReadSingle();
+                    positionData.Longitude = binaryReader.ReadSingle();
+                    positionData.RecordedTimeUTC = binaryReader.ReadUInt64();
+
+                    vehiclePositions.Add(positionData);
                 }
             }
-            catch (Exception generalException)
-            {
-                Console.WriteLine(generalException.Message);
-            }
-
-            return vehiclePositionList;
         }
+
+        return vehiclePositions;
     }
+
+    private static string GetStringFromBinary(BinaryReader readerInstance)
+    {
+        var byteDataList = new List<byte>();
+        byte currentByte;
+        while ((currentByte = readerInstance.ReadByte()) != 0)
+        {
+            byteDataList.Add(currentByte);
+        }
+
+        return Encoding.UTF8.GetString(byteDataList.ToArray());
+    }
+
+}
